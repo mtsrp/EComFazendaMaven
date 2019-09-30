82093b0cf2401d2e3c49b31a3cf67d45f7e7ce2e
mtsrp <matheus.rocha.pimenta@hotmail.com>
2019-09-29 21:59:59 -0300

    First Commit Maven

---
+++ src/main/java/com/classes/ClassePrincipal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.classes;
+
+import com.dao.ClienteDAO;
+import com.entidade.ClienteEntidade;
+
+/**
+ *
+ * @author mathe
+ */
+public class ClassePrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ClienteDAO cliDao = new ClienteDAO();
+        ClienteEntidade cli = new ClienteEntidade(1, "Matheus Rocha");
+        
+        cliDao.insert(cli);
+        
+        
+    }
+    
+}
